765c42fdec69786c8f8b7dbd94b045c9f91543d9
emanuelesguerzo <sguerzo.emanuele@gmail.com>
2025-04-17 03:27:22 +0200

    feat: move player + using groups to manage objects

--- main.py
+++ main.py
@@ -8,21 +8,36 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
+    
+    # Groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    # Delta Time
+    dt = 0
+
+    # Game Loop
     while True:
         for event in pygame.event.get():
-            player.draw(screen)
             if event.type == pygame.QUIT:
                 return
             
-        screen.fill("black")
-        player.draw(screen)
-        pygame.display.flip()
-        dt = clock.tick(60) / 1000
+        updatable.update(dt)  
 
+        screen.fill("black")
+
+        # Drawing Objects
+        for object in drawable:
+            object.draw(screen)
+
+        pygame.display.flip()
+
+        # Limit Framerate to 60 FPS
+        dt = clock.tick(60) / 1000 
 
 if __name__ == "__main__":
     main()
